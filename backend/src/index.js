11cf8b510f7d6d81dfd72c5fbb5263bdbd434ff8
Reino <reino@prospercart.fi>
2017-11-15 00:31:20 +0200

    muutoksia

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ const cors = require('kcors');
 
 const appId = process.env.APPID || 'ae20e0a09a4c73aa871a96f99007238f';
 const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
-const targetCity = process.env.TARGET_CITY || "Tripoli,ly";
+const targetCity = process.env.TARGET_CITY || "Tampere,fi";
 
 const port = process.env.PORT || 9000;
 
