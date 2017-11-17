565e58e91b484e098bae6dcf9b03037f63566aac
Reino <reino@prospercart.fi>
2017-11-17 22:26:31 +0200

    lint fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,4 @@
-const debug = require('debug')('weathermap');
+// const debug = require('debug')('weathermap');
 
 const Koa = require('koa');
 const router = require('koa-router')();
@@ -6,8 +6,8 @@ const fetch = require('node-fetch');
 const cors = require('kcors');
 
 const appId = process.env.APPID || 'ae20e0a09a4c73aa871a96f99007238f';
-const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
-const targetCity = process.env.TARGET_CITY || "Tampere,fi";
+const mapURI = process.env.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5';
+const targetCity = process.env.TARGET_CITY || 'Tampere,fi';
 
 const port = process.env.PORT || 9000;
 
@@ -19,7 +19,7 @@ const fetchWeather = async () => {
   const endpoint = `${mapURI}/weather?q=${targetCity}&appid=${appId}&`;
   const response = await fetch(endpoint);
 
-  return response ? response.json() : {}
+  return response ? response.json() : {};
 };
 
 router.get('/api/weather', async ctx => {
